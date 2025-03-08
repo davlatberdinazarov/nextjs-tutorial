8f9cfd2f2aec8c0e417d54028b7223bc0ea023c6
davlatberdinazarov <aberdinazarov71@gmail.com>
2025-03-08 09:58:10 +0500

    modified data

--- app/(root)/crud/page.tsx
+++ app/(root)/crud/page.tsx
@@ -1,36 +1,75 @@
+// // app/users/ssr/page.tsx
+
+// import UsersList, { UserProps } from "@/components/users-list";
+
+
+// async function getServerUsers() {
+//   const res = await fetch("http://localhost:3002/api/users", {
+//     cache: "no-store",
+//   });
+//   return res.json();
+// }
+
+// export default async function UsersPageSSR() {
+//   const users = await getServerUsers();
+
+//   return (
+//     <div className="max-w-4xl mx-auto">
+//       <h1 className="text-center text-xl font-bold mt-5">
+//         Users List (SSR - Server Side Rendering)
+//       </h1>
+//       <UsersList users={users} />
+//     </div>
+//   );
+// }
+
+
+// async function getStaticUsers() {
+//   const res = await fetch("http://localhost:3002/api/users", {
+//     next: { revalidate: 60 }, // 60 soniyada yangilash
+//   });
+//   return res.json();
+// }
+
+// export default async function UsersPageSSG() {
+//   const users = await getStaticUsers();
+
+//   return (
+//     <div className="max-w-4xl mx-auto">
+//       <h1 className="text-center text-xl font-bold mt-5">
+//         Users List (SSG - Static Site Generation)
+//       </h1>
+//       <UsersList users={users} />
+//     </div>
+//   );
+// }
+
+// app/users/csr/page.tsx
 "use client";
 
 import { useEffect, useState } from "react";
+import UsersList, { UserProps } from "@/components/users-list";
 
-export interface UserProps {
-  _id: string;
-  name: string;
-  email: string;
-  password: string | number;
-}
+export default function UsersPageCSR() {
+  const [users, setUsers] = useState<UserProps[]>([]);
+  const [loading, setLoading] = useState(true);
 
-// 🔹 **SSG (Static Site Generation)**
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:3002/api/users");
-  const users = await res.json();
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
 
-  return {
-    props: { users },
-    revalidate: 60, // 60 soniyada yangilash
-  };
-}
-
-// 🔹 **SSR (Server-side Rendering)**
-async function getServerUsers() {
-  const res = await fetch("http://localhost:3002/api/users", {
-    cache: "no-store",
-  });
-  return res.json();
-}
-
-export default function Crud() {
   return (
-    <div>Crud</div>
-  )
+    <div className="max-w-4xl mx-auto">
+      <h1 className="text-center text-xl font-bold mt-5">
+        Users List (CSR - Client Side Rendering)
+      </h1>
+      {loading ? <p>Loading...</p> : <UsersList users={users} />}
+    </div>
+  );
 }
 
